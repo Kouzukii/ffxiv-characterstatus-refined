5cf304f1247b7f8cbe900d35f4cd1d9852467a7b
Kouzukii <ludewig.b@gmail.com>
2024-11-22 19:16:49 +0100

    Fix Excel Item Offsets

--- CharacterPanelRefined/Equations.cs
+++ CharacterPanelRefined/Equations.cs
@@ -125,9 +125,9 @@ public class Equations {
         try {
             var lvl = uiState->PlayerState.CurrentLevel;
             var ap = uiState->PlayerState.Attributes[(int)(jobId.IsCaster() ? Attributes.AttackMagicPotency : Attributes.AttackPower)];
-            var inventoryItemData = (ushort*)((IntPtr)InventoryManager.Instance() + 9272);
-            var weaponBaseDamage = /* phys/magic damage */ inventoryItemData[jobId.IsCaster() ? 17 : 16] + /* hq bonus */ inventoryItemData[29];
-            if (ilvlSync != null && ( /* equip lvl */ inventoryItemData[35] > lvl || ilvlSyncType == IlvlSyncType.Strict)) {
+            var inventoryExcelData = (ushort*)((IntPtr)InventoryManager.Instance() + 9360);
+            var weaponBaseDamage = /* phys/magic damage */ inventoryExcelData[jobId.IsCaster() ? 21 : 20] + /* hq bonus */ inventoryExcelData[33];
+            if (ilvlSync != null && ( /* equip lvl */ inventoryExcelData[39] > lvl || ilvlSyncType == IlvlSyncType.Strict)) {
                 if (cachedIlvl?.Ilvl != ilvlSync)
                     cachedIlvl = (ilvlSync.Value, Service.DataManager.GetExcelSheet<ItemLevel>().GetRow(ilvlSync.Value).PhysicalDamage);
                 weaponBaseDamage = Math.Min(cachedIlvl.Value.Dmg, weaponBaseDamage);
